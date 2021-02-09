d92105132b4d1f23d9298eece87687ee6801e092
= <=>
2021-02-09 14:09:11 +0100

    Updates

---
+++ precache-manifest.f70cbbb24e07286e396452974e68b34a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0c61ff86febb9122ec03ba3780e1d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83615de33a2ecd8165cb",
+    "url": "/static/css/main.978356ea.chunk.css"
+  },
+  {
+    "revision": "db91154cce432194c19c",
+    "url": "/static/js/2.62527598.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62527598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83615de33a2ecd8165cb",
+    "url": "/static/js/main.df549a5d.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "/static/media/Montserrat-Bold.9c71d42b.otf"
+  },
+  {
+    "revision": "92db9a0772b3732e6d686fec3711af42",
+    "url": "/static/media/Montserrat-Regular.92db9a07.otf"
+  },
+  {
+    "revision": "bb3740d350b0186ce32b5678972bf061",
+    "url": "/static/media/Montserrat-SemiBold.bb3740d3.otf"
+  }
+]);
